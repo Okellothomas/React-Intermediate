a38aa72dd5c82e48739a7273716b244d86fdd3e5
Okellothomas <thomasolal33@gmail.com>
2023-02-09 19:24:07 +0300

    add the index.css

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "..index.css"
+
+export default function Footer() {
+    return (
+        <div>
+            <p className="footer">Okello Thomas @2022 All rights reserved!</p>
+        </div>
+    )
+}
