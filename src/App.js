a67d9c0e64098a2e73b10708f247389e70e9b8f7
Okellothomas <thomasolal33@gmail.com>
2023-02-09 23:04:05 +0300

    image implementation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import MyNavbar from "./components/navbar";
 import MyCarousel from "./components/carousel";
 import Footer from "./components/footer";
+import Body from "./components/body";
 
 export default function App() {
     return (
         <div>
             <MyNavbar/>
             <MyCarousel />
+            <Body />
             <Footer/>
         </div>
     )
---
+++ src/components/body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css"
+
+export default function Body() {
+    return (
+        <div className="bd">
+            <img src="../images/skate.jpg" />
+            <hr />
+            <h1>Fun fact about react</h1>
+        </div>
+    )
+}
